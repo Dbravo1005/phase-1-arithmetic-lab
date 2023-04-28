7b690feaf1f65b8e0f73653e87e8f6a42ded2c7d
Damian Bravo <Dbravo1005@gmail.com>
2023-04-27 21:52:26 -0400

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = (num1 * num2);
+//
+let random = 1
+Math.random((random) * 3) + 1;
+//
+let num3 = 4;
+let num4 = 9;
+
+let mod = (num3 % num4);
+//
+
+let max = (3, 5, 6, 17, 20);
+Math.min(max);
+
+
+
